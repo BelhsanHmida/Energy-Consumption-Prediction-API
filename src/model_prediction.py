ec99fbdc420c1a3b6c441cbd01c746ac3e189134
BelhsanHmida <mohamedbelhsanhmida@gmail.com>
2024-06-04 07:54:27 +0100

    Update model_prediction

--- src/model_prediction.py
+++ src/model_prediction.py
@@ -7,55 +7,55 @@ The primary functions provided in this module are `load_model` and `predict`.
 
 Example usage:
     >>> from model_prediction import load_model, predict
-    >>> model = load_model('models/energy_forecast_model.pkl')
-    >>> forecast = predict(model, periods=10)
+    >>> model = load_model('models/energy_forecast_xgb_model.pkl')
+    >>> forecast = predict(model, X_input)
 """
 
 import pickle
-from prophet import Prophet
 import pandas as pd
+from xgboost import XGBRegressor
+from utils import create_features
+from data_ingestion import load_data
 
-
-def load_model(file_path: str) -> Prophet:
+def load_model(file_path: str) -> XGBRegressor:
     """
     Loads a trained model from a given file path.
 
-    Args:
-        file_path (str): The path to the file containing the saved model.
-
-    Returns:
-        Prophet: The loaded model.
-
-    Example:
-        >>> model = load_model('models/energy_forecast_model.pkl')
     """
     with open(file_path, 'rb') as f:
         model = pickle.load(f)
     return model
 
-
-def predict(model: Prophet, periods: int) -> pd.DataFrame:
+def predict(model: XGBRegressor, X_input: pd.DataFrame) -> pd.DataFrame:
     """
-    Generates future predictions using the loaded model.
+    Generates predictions using the loaded model.
 
     Args:
-        model (Prophet): The trained model to use for making predictions.
-        periods (int): The number of periods (e.g., hours, days) to forecast.
+        model (XGBRegressor): The trained model to use for making predictions.
+        X_input (pd.DataFrame): The input features for making predictions.
 
     Returns:
-        pd.DataFrame: The forecasted data as a pandas DataFrame.
+        pd.DataFrame: The predicted values.
 
     Example:
-        >>> forecast = predict(model, periods=10)
+        >>> forecast = predict(model, X_input)
     """
-    future = model.make_future_dataframe(periods=periods)
-    forecast = model.predict(future)
-    return forecast
-
+    predictions = model.predict(X_input)
+    return pd.DataFrame(predictions, columns=['prediction'])
 
 if __name__ == "__main__":
     # Example usage
-    file_path = 'models/energy_forecast_model.pkl'
+    file_path = 'models/xgboost_energy_model01.pkl'
     model = load_model(file_path)
-    forecast = predict(model, periods=10)
-    print(forecast.head())
+
+    # Load and preprocess the data
+    data = load_data("data/SF_hospital_load.csv")
+    
+    # Create features and target variable
+    X, y = create_features(data, label='y')
+    
+    # Predict the future periods (example: next 10 periods)
+    X_input = X.tail(10)  # Replace this with appropriate feature creation for future periods
+    forecast = predict(model, X_input)
+    
+    print(forecast)
