9152507850939ae33e2f0f13f6f8afbc2f32c3a7
BelhsanHmida <mohamedbelhsanhmida@gmail.com>
2024-06-04 07:55:38 +0100

    Update utils

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,19 @@
-import pandas as pd
+import requests
+import pandas as pd 
+
+def download_file(url: str, local_filename: str):
+    """
+    Downloads a file from the given URL and saves it locally.
+
+    Args:
+        url (str): The URL of the file to download.
+        local_filename (str): The local path where the file will be saved.
+    """
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
 
 def create_features(df: pd.DataFrame, label: str = None) -> pd.DataFrame:
     """
@@ -30,5 +45,3 @@ def create_features(df: pd.DataFrame, label: str = None) -> pd.DataFrame:
         return X, y
     
     return X
-
-
